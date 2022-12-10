e154cd90d5f5854079ec695570cfbf0814597fa6
Briana Berger <brianamb@stanford.edu>
2022-12-10 00:27:50 +0000

    Cleaned up research_functions to work with Keegan's category/country graphing edits

--- research_functions.py
+++ research_functions.py
@@ -16,25 +16,26 @@ def get_version_str_end(service_name):
 
 # If any port's service is vulnerable, the IP is considered vulnerable.
 # Only if all of the port's services are patched, the IP is considered patched.
-def get_patching_history(ip_to_history, service_name, patching_versions):
+def get_binary_history(ip_to_history, service_name, versions):
     ip_to_port_ver_history = get_port_ver_history(ip_to_history, service_name, patching_versions)
-    ip_to_patch_history = {}
+    ip_to_binary_history = {}
     for ip in ip_to_port_ver_history.keys():
         for date in ip_to_port_ver_history[ip].keys():
             ports_to_version = ip_to_port_ver_history[ip][date]
-            isPatched = True
+            isInVersions = True
             for port in ports_to_version.keys():
-                if ports_to_version[port] not in patching_versions:
-                    isPatched = False
-                    if ip not in ip_to_patch_history:
-                        ip_to_patch_history[ip] = {}
-                    ip_to_patch_history[ip][date] = 0
-            if len(ports_to_version) > 0 and isPatched:
-                if ip not in ip_to_patch_history:
-                    ip_to_patch_history[ip] = {}
-                ip_to_patch_history[ip][date] = 1
-    return ip_to_patch_history
-            
+                if ports_to_version[port] not in versions:
+                    isInVersions = False
+                    if ip not in ip_to_binary_history:
+                        ip_to_binary_history[ip] = {}
+                    ip_to_binary_history[ip][date] = 0
+            if len(ports_to_version) > 0 and isInVersions:
+                if ip not in ip_to_binary_history:
+                    ip_to_binary_history[ip] = {}
+                ip_to_binary_history[ip][date] = 1
+    return ip_to_binary_history
+
+# Parses through ip_to_history to accurately categorize the server via port-granularity           
 def get_port_ver_history(ip_to_history, service_name, patching_versions):
     ip_to_port_ver_history = {}
     for ip in ip_to_history.keys():
@@ -93,131 +94,76 @@ def extract_version(body, service_name, ip):
     if result > 0:
         end = body.find(end_string, result + len(search_string))
         return body[result + len(search_string) : end]
-    # TODO no version was thrown during Rundeck - maybe see what server didn't have version
     raise Exception("No verion was found for " + ip + " and here was the body: " + body)
                 
-def get_historical_data(h, patched_ips, service_name):
+# Gets a dict from ip to a dict of dates to that ip's response for that date
+def get_historical_data(h, ips, service_name, file_name):
     datetimes = [datetime.date(2021, 12, 6), datetime.date(2022, 1, 6), datetime.date(2022, 2, 6), datetime.date(2022, 3, 6), datetime.date(2022, 4, 6), datetime.date(2022, 5, 6), datetime.date(2022, 6, 6), datetime.date(2022, 7, 6), datetime.date(2022, 8, 6), datetime.date(2022, 9, 6), datetime.date(2022, 10, 6), datetime.date(2022, 11, 6), datetime.date(2022, 12, 5)]
     dates = [d.strftime('%Y-%m-%dT%H:%M:%SZ') for d in datetimes]
     ip_to_history = {}
     for date in dates:
-        responses = h.bulk_view(patched_ips, at_time = date)
+        responses = h.bulk_view(ips, at_time = date)
         for ip in responses.keys():
             resp = responses[ip]
             if ip not in ip_to_history:
                 ip_to_history[ip] = {}
             ip_to_history[ip][date] = resp
     
-    with open(service_name + "_" + "history.json", 'w') as history_file:
+    with open(file_name, 'w') as history_file:
         history_file.write(json.dumps(ip_to_history))
     return ip_to_history
 
-def get_categories(h, queries, isPatched):
-    patched_ips = []
-    ases = []
-    num = 0
+# Gets all of the current IPs associated with each query in queries
+def get_ips(h, queries):
+    ips = []
     for query in queries:
         for page in h.search(query, pages=-1):
-            num += len(page)
             for p in page:
-                if isPatched and "ip" in p:
-                    patched_ips.append(p["ip"])
-                if "autonomous_system" in p:
-                    if "asn" in p['autonomous_system']:
-                        ases.append(p['autonomous_system']['asn'])
-                    
-    # Searches for categories associated with vulnerable instances' ASes
-    with open(r'2022-05_categorized_ases.csv', newline='\n') as file:
-        cat_ases = csv.DictReader(file)
-        cats = {}
-        for entry in cat_ases:
-            for asn in ases:
-                if asn == int(entry['ASN'][2:]):
-                    if entry['Category 1 - Layer 1'] == "Computer and Information Technology" and entry['Category 2 - Layer 1'] != '':
-                        layer = 'Category 1 - Layer 2'
-                    else:
-                        layer = 'Category 1 - Layer 1'
-                        
-                    if entry[layer] in cats:
-                        cats[entry[layer]] += 1
-                    else:
-                        cats[entry[layer]] = 1 
+                if "ip" in p:
+                    ips.append(p["ip"])
+    return ips
 
-    # Print out top 10 vulnerable categories
-    top10 = heapq.nlargest(10, cats, key=cats.get)
-    top10_to_count = {}
-    for cat in top10:
-        top10_to_count[cat] = cats[cat]
-    return [top10_to_count, num, patched_ips]
-        
+# Combines get_ips and get_historical_data while also checking if that file already exists
+def get_history(h, queries, service, isPatched):
+    ips = get_ips(h, queries)
+    file_name = service + "_patched_history.json" if isPatched else service + "_vulnerable_history.json"
+    
+    file_exists = exists(file_name)
+    if not file_exists:
+        ip_to_history = get_historical_data(h, patched_ips, service, file_name)
+    else: 
+        f = open(file_name)
+        ip_to_history = json.load(f)
+        f.close()
+    return ip_to_history
 
-def get_countries(c, queries):
-    countryToNum = {}
-    for query in queries:
-            report = c.v2.hosts.aggregate(
-                query,
-                "location.country",
-                num_buckets=195,
-            )
+# Gets the total number of ips associated with dict (i.e. how many vulnerable IPs are there?)
+def get_total_query_size(ip_to_history):
+    return len(ip_to_history.keys())
 
-            if 'buckets' in report:
-                buckets = report['buckets']
-                for elem in buckets:
-                    if elem['key'] in countryToNum:
-                        countryToNum[elem['key']] += elem['count']
-                    else:
-                        countryToNum[elem['key']] = elem['count']
-                        
-    top10 = heapq.nlargest(10, countryToNum, key=countryToNum.get)
-    top10_to_count = {}
-    for country in top10:
-        top10_to_count[country] = countryToNum[country]
-    return top10_to_count
-
+# Gets total number running a particular service
 def get_total_running_service(h, query):
     num = 0
     for page in h.search(query, pages=-1):
         num += len(page)
     return num  
     
-def print_data(c, h, service, vuln_queries, patch_queries, query, patch_versions):
-    # print(f"## Top 10 Countries with Vulnerable Logj4 Instances of {service} ##")
-    # print(get_countries(c, vuln_queries))
-    # print("\n")
-    # print(f"## Top 10 Countries with Patched Logj4 Instances of {service} ##")
-    # print(get_countries(c, patch_queries))
-    # print("\n")
-
-    print(f"## Top 10 Categories with Vulnerable Logj4 Instances of {service} ##")
-    result_vuln = get_categories(h, vuln_queries, False)
-    print(result_vuln[0])
-    print("\n")
-    print(f"## Top 10 Categories with Patched Logj4 Instances of {service} ##")
-    result_patch = get_categories(h, patch_queries, True)
-    print(result_patch[0])
-    print("\n")
+def print_data(c, h, service, vuln_queries, patch_queries, query, patch_versions, vuln_versions):
+    # Get vulnerable and patched history
+    ip_to_vuln_history = get_history(h, vuln_queries, service, False)
+    ip_to_patched_history = get_history(h, patch_queries, service, True)
     
-    # patched_ips = result_patch[2]
-    # file_exists = exists(service + "_" + "history.json")
-    # if not file_exists:
-    #     ip_to_history = get_historical_data(h, patched_ips, service)
-    # else: 
-    #     print("SHOULD BE HERE")
-    #     f = open(service + "_" + "history.json")
-    #     ip_to_history = json.load(f)
-    #     f.close()
+    # Get binary representation of patching history for both curr vuln and patched IPs
+    ip_to_patched_binary_history = get_binary_history(ip_to_patched_history, service, patch_versions)
+    ip_to_vuln_binary_history = get_binary_history(ip_to_vuln_history, service, vuln_versions)
     
-    # isPatchedHistory = get_patching_history(ip_to_history, service, patch_versions)
-    # print(isPatchedHistory)
-    # DO STUFF WITH ip_to_history
+    print(f"## Total Vulnerable Logj4 Instances of {service} ##")
+    print(get_total_query_size(ip_to_vuln_history))
+    print("\n")
+    print(f"## Total Patched Logj4 Instances of {service} ##")
+    print(get_total_query_size(ip_to_patched_history))
+    print("\n")
 
-    # print(f"## Total Vulnerable Logj4 Instances of {service} ##")
-    # print(result_vuln[1])
-    # print("\n")
-    # print(f"## Total Patched Logj4 Instances of {service} ##")
-    # print(result_patch[1])
-    # print("\n")
-
-    # print(f"## Total Instances of {service} ##")
-    # print(str(get_total_running_service(h, query)))
-    # print("\n")
+    print(f"## Total Instances of {service} ##")
+    print(str(get_total_running_service(h, query)))
+    print("\n")
