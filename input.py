93d9a29293223da119a964a0a7418ab979598a8f
Briana Berger <brianamb@stanford.edu>
2022-12-11 18:30:20 +0000

    Run one service at a time

--- input.py
+++ input.py
@@ -15,14 +15,14 @@ def solr_query(version):
     return f"same_service(services.http.response.html_title=`Solr Admin` and services.http.response.body: `img/favicon.ico?_={version}`)"
 vulnerable_solr_vers_queries = [solr_query(version) for version in get_history.vulnerable_solr_vers]
 patched_solr_vers_queries = [solr_query(version) for version in get_history.patched_solr_vers]
-get_history.print_data(h, "Solr Admin", vulnerable_solr_vers_queries, patched_solr_vers_queries, "services.http.response.html_title=`Solr Admin`")
+#get_history.print_data(h, "Solr Admin", vulnerable_solr_vers_queries, patched_solr_vers_queries, "services.http.response.html_title=`Solr Admin`")
 
 # Get Pagerduty data
 def pd_query(version):
     return f"same_service(services.http.response.html_title: `Rundeck` and services.http.response.body:`https://docs.rundeck.com/{version}`)"
 vulnerable_pd_vers_queries = [pd_query(version) for version in get_history.vulnerable_pd_vers]
 patched_pd_vers_queries = [pd_query(version) for version in get_history.patched_pd_vers]
-get_history.print_data(h, "Rundeck", vulnerable_pd_vers_queries, patched_pd_vers_queries, "services.http.response.html_title: `Rundeck`")
+#get_history.print_data(h, "Rundeck", vulnerable_pd_vers_queries, patched_pd_vers_queries, "services.http.response.html_title: `Rundeck`")
 
 # Get Neo4j data
 def neo4j_query(version):
